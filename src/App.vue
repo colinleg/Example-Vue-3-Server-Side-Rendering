183bcb8fcd4e3e815fc50c07beb8df3ef71e8695
Colin Legoedec <clegoedec@gmail.com>
2022-02-07 09:32:02 +0100

    initial Commit

---
+++ src/App.vue
@@ -0,0 +1,33 @@
+<template>
+  <div id="app">
+    <img alt="Vue logo" src="./assets/logo.png">
+    <div> Bienvenue dans le monde du SSR.. </div>
+  </div>
+</template>
+
+<script>
+
+
+
+export default {
+  name: 'App',
+  data(){
+    return {
+      a: 0
+    }
+  }
+}
+</script>
+
+<style>
+
+#app {
+  font-family: Avenir, Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+  font-size: 3rem;
+  margin-top: 60px;
+}
+</style>
